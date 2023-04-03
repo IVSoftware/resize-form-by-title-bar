1b48507bb3f8c06d0718cbbd3fb3dcd974a70956
Tom Gregor <tgregor@ivsoftware.com>
2023-04-03 07:27:40 -0600

    drag-resize

--- resize-form-by-title-bar/Form1.Designer.cs
+++ resize-form-by-title-bar/Form1.Designer.cs
@@ -28,34 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            titleBarPanel = new System.Windows.Forms.Panel();
+            titleBarPanel = new System.Windows.Forms.TableLayoutPanel();
+            labelTitleBar = new System.Windows.Forms.Label();
+            labelResize = new System.Windows.Forms.Label();
+            labelMove = new System.Windows.Forms.Label();
+            radioButtonMove = new System.Windows.Forms.RadioButton();
+            radioButtonResize = new System.Windows.Forms.RadioButton();
+            groupBox = new System.Windows.Forms.GroupBox();
+            titleBarPanel.SuspendLayout();
+            groupBox.SuspendLayout();
             SuspendLayout();
             // 
             // titleBarPanel
             // 
             titleBarPanel.BackColor = System.Drawing.Color.Azure;
+            titleBarPanel.ColumnCount = 3;
+            titleBarPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            titleBarPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            titleBarPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            titleBarPanel.Controls.Add(labelTitleBar, 0, 0);
+            titleBarPanel.Controls.Add(labelResize, 1, 0);
+            titleBarPanel.Controls.Add(labelMove, 2, 0);
             titleBarPanel.Dock = System.Windows.Forms.DockStyle.Top;
             titleBarPanel.Location = new System.Drawing.Point(0, 0);
             titleBarPanel.Name = "titleBarPanel";
-            titleBarPanel.Size = new System.Drawing.Size(800, 58);
+            titleBarPanel.RowCount = 1;
+            titleBarPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            titleBarPanel.Size = new System.Drawing.Size(500, 50);
             titleBarPanel.TabIndex = 0;
+            // 
+            // labelTitleBar
+            // 
+            labelTitleBar.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            labelTitleBar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            labelTitleBar.Location = new System.Drawing.Point(3, 0);
+            labelTitleBar.Name = "labelTitleBar";
+            labelTitleBar.Size = new System.Drawing.Size(394, 50);
+            labelTitleBar.TabIndex = 0;
+            labelTitleBar.Text = "Custom Title Bar";
+            labelTitleBar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelResize
+            // 
+            labelResize.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            labelResize.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            labelResize.Location = new System.Drawing.Point(403, 0);
+            labelResize.Name = "labelResize";
+            labelResize.Size = new System.Drawing.Size(44, 50);
+            labelResize.TabIndex = 0;
+            labelResize.Text = "*";
+            labelResize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelMove
+            // 
+            labelMove.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            labelMove.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            labelMove.Location = new System.Drawing.Point(453, 0);
+            labelMove.Name = "labelMove";
+            labelMove.Size = new System.Drawing.Size(44, 50);
+            labelMove.TabIndex = 0;
+            labelMove.Text = "*";
+            labelMove.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButtonMove
+            // 
+            radioButtonMove.AutoSize = true;
+            radioButtonMove.Checked = true;
+            radioButtonMove.Location = new System.Drawing.Point(23, 48);
+            radioButtonMove.Name = "radioButtonMove";
+            radioButtonMove.Size = new System.Drawing.Size(82, 29);
+            radioButtonMove.TabIndex = 1;
+            radioButtonMove.TabStop = true;
+            radioButtonMove.Text = "Move";
+            radioButtonMove.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonResize
+            // 
+            radioButtonResize.AutoSize = true;
+            radioButtonResize.Location = new System.Drawing.Point(140, 48);
+            radioButtonResize.Name = "radioButtonResize";
+            radioButtonResize.Size = new System.Drawing.Size(85, 29);
+            radioButtonResize.TabIndex = 1;
+            radioButtonResize.Text = "Resize";
+            radioButtonResize.UseVisualStyleBackColor = true;
+            // 
+            // groupBox
+            // 
+            groupBox.Controls.Add(radioButtonMove);
+            groupBox.Controls.Add(radioButtonResize);
+            groupBox.Location = new System.Drawing.Point(12, 77);
+            groupBox.Name = "groupBox";
+            groupBox.Size = new System.Drawing.Size(250, 110);
+            groupBox.TabIndex = 2;
+            groupBox.TabStop = false;
+            groupBox.Text = "Default Title Bar Function";
             // 
             // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
+            ClientSize = new System.Drawing.Size(500, 300);
+            Controls.Add(groupBox);
             Controls.Add(titleBarPanel);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             MinimumSize = new System.Drawing.Size(50, 50);
             Name = "Form1";
             StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             Text = "Form1";
+            titleBarPanel.ResumeLayout(false);
+            groupBox.ResumeLayout(false);
+            groupBox.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel titleBarPanel;
+        private System.Windows.Forms.TableLayoutPanel titleBarPanel;
+        private System.Windows.Forms.Label labelTitleBar;
+        private System.Windows.Forms.Label labelResize;
+        private System.Windows.Forms.Label labelMove;
+        private System.Windows.Forms.RadioButton radioButtonMove;
+        private System.Windows.Forms.RadioButton radioButtonResize;
+        private System.Windows.Forms.GroupBox groupBox;
     }
 }
--- resize-form-by-title-bar/Form1.cs
+++ resize-form-by-title-bar/Form1.cs
@@ -1,58 +1,138 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace resize_form_by_title_bar
 {
+    enum DragAction{ None, Move, Resize, }
     public partial class Form1 : Form, IMessageFilter
     {
         private Point _dragStartScreen;
+        private Point _dragStartLocation;
         private Size _dragStartSize;
-        private bool _dragResizing = false;
 
         public Form1()
         {
             InitializeComponent();
             Application.AddMessageFilter(this);
             Disposed += (sender, e) => Application.RemoveMessageFilter(this);
-            MinimumSize = new Size(50, 50); // Make sure it can't disappear!
+            MinimumSize = new Size(260, 200); // Make sure it can't disappear!
         }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            #region G L Y P H
+            var path = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Fonts",
+                "glyphs.ttf");
+            privateFontCollection.AddFontFile(path);
+            var fontFamily = privateFontCollection.Families[0];
+            Glyphs = new Font(fontFamily, 11F);
+            #endregion G L Y P H
+            labelResize.Font = Glyphs;
+            labelResize.Text = "\uE800";
+            labelResize.ForeColor = Color.MediumSeaGreen;
+            labelMove.Font = Glyphs;
+            labelMove.Text = "\uE801";
+            labelMove.ForeColor = Color.MediumSeaGreen;
+        }
+        public static Font Glyphs { get; private set; }
+        PrivateFontCollection privateFontCollection = new PrivateFontCollection();
+
         const int WM_LBUTTONDOWN = 0x0201;
         const int WM_MOUSEMOVE = 0x0200;
         const int WM_LBUTTONUP = 0x0202;
+
+        DragAction _dragAction = default(DragAction);
+        DragAction DragAction
+        {
+            get => _dragAction;
+            set
+            {
+                if (!Equals(_dragAction, value))
+                {
+                    _dragAction = value;
+                    switch (DragAction)
+                    {
+                        case DragAction.None:
+                            Cursor = Cursors.Default;
+                            break;
+                        case DragAction.Move:
+                            Cursor = Cursors.SizeAll;
+                            _dragStartScreen = MousePosition;
+                            _dragStartLocation = Location;
+                            break;
+                        case DragAction.Resize:
+                            Cursor = Cursors.SizeNESW;
+                            _dragStartScreen = MousePosition;
+                            _dragStartSize = Size;
+                            break;
+                        default:
+                            break;
+                    }
+                    Debug.WriteLine(DragAction);
+                }
+            }
+        }
 
         public bool PreFilterMessage(ref Message m)
         {
             switch (m.Msg)
             {
                 case WM_LBUTTONDOWN:
-                    var client = PointToClient(MousePosition);
-                    if (titleBarPanel.ClientRectangle.Contains(client))
+                    var client = titleBarPanel.PointToClient(MousePosition);
+                    if (labelMove.Bounds.Contains(client))
                     {
-                        _dragResizing = true;
-                        _dragStartScreen = MousePosition;
-                        _dragStartSize = Size;
-                        Capture = true;
-                        m.Result = (IntPtr)1;
-                        return true;
+                        DragAction = DragAction.Move;
                     }
-                    break;
-                case WM_MOUSEMOVE:
-                    if (_dragResizing)
+                    else if (labelResize.Bounds.Contains(client))
                     {
-                        int
-                            deltaX = MousePosition.X - _dragStartScreen.X,
-                            deltaY = MousePosition.Y - _dragStartScreen.Y;
-                        BeginInvoke(new Action(() =>
-                        {
-                            Size = new Size(_dragStartSize.Width + deltaX, _dragStartSize.Height + deltaY);
-                        }));
-                        m.Result = (IntPtr)1;
-                        return true;
+                        DragAction = DragAction.Resize;
+                    }
+                    else if (titleBarPanel.ClientRectangle.Contains(client))
+                    {
+                        DragAction =
+                            radioButtonMove.Checked ?
+                                DragAction.Move :
+                                DragAction.Resize;
+                    }
+                    else break;
+                    Capture = true;
+                    m.Result = (IntPtr)1;
+                    return true;
+                case WM_MOUSEMOVE:
+                    int
+                        deltaX = MousePosition.X - _dragStartScreen.X,
+                        deltaY = MousePosition.Y - _dragStartScreen.Y;
+                    switch (DragAction)
+                    {
+                        case DragAction.None:
+                            break;
+                        case DragAction.Move:
+                            BeginInvoke(new Action(() =>
+                            {
+                                Location = new Point(_dragStartLocation.X + deltaX, _dragStartLocation.Y + deltaY);
+                            }));
+                            m.Result = (IntPtr)1;
+                            break;
+                        case DragAction.Resize:
+                            BeginInvoke(new Action(() =>
+                            {
+                                Size = new Size(_dragStartSize.Width + deltaX, _dragStartSize.Height + deltaY);
+                            }));
+                            m.Result = (IntPtr)1;
+                            return true;
+                        default:
+                            break;
                     }
                     break;
                 case WM_LBUTTONUP:
-                    _dragResizing = false;
+                    DragAction = DragAction.None;
                     Capture = false;
                     break;
             }
